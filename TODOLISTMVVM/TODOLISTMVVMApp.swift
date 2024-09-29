fe7b154d546741d47d8ad1901e77dfab6edeaec9
Arsalan Wahid Asghar <arsalanwahid1993@gmail.com>
2024-09-29 14:32:08 +0500

    Initial Commit

---
+++ TODOLISTMVVM/TODOLISTMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  TODOLISTMVVMApp.swift
+//  TODOLISTMVVM
+//
+//  Created by Arsalan Wahid Asghar on 9/29/24.
+//
+
+import SwiftUI
+
+@main
+struct TODOLISTMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
